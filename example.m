51d0b2b137f9ce543daa684c7026a1defc20b88e
felixig <fiv@gmx.at>
2019-04-12 15:10:23 +0200

    Add files via upload

--- example.m
+++ example.m
@@ -2,11 +2,11 @@
 load exampledata.mat
 fprintf("SDO, twodimensional example \n");
 figure(1)
-scatter(dataPoints(:,1),dataPoints(:,2),5,label);
+scatter(dataPoints(:,1),dataPoints(:,2),2,label);
 title('Datapoint color based on: class (1, 2, 3 or 4) and outliers')
 fprintf("Calling SDO with default parameters...");
 [ y, observers, param ] = sdof( dataPoints );
 figure(2)
-scatter(dataPoints(:,1),dataPoints(:,2),5,y);
+scatter(dataPoints(:,1),dataPoints(:,2),2,y);
 title('Datapoint color based SDO outlierness rank');
 colorbar
